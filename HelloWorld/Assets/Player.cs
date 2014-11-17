6a40f67e411e56d5370694ee0445692a3b986470
heldersepu <heldersepu@gmail.com>
2014-11-17 01:27:27 +0000

    My first Project in Unity. Just a rotating Cube and a moving sphere that appears to bounce of the cube

---
+++ HelloWorld/Assets/Player.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	private Transform _playerTransf;
+	// Use this for initialization
+	void Start () {
+		var player = this.GetComponent<Player>();
+		_playerTransf = player.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_playerTransf.Rotate (0, 0, 2);
+	}
+}
