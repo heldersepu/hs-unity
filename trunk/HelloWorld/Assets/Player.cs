fd384caf030f6e30473292a3872a4dc4f8cf6c2e
heldersepu@gmail.com <heldersepu@gmail.com@72dac0d8-bfa9-6037-f185-37c1354a5d2f>
2014-11-19 01:06:46 +0000

    Some Improvements to HelloWorld

--- trunk/HelloWorld/Assets/Player.cs
+++ trunk/HelloWorld/Assets/Player.cs
@@ -2,15 +2,17 @@
 using System.Collections;
 
 public class Player : MonoBehaviour {
-	private Transform _playerTransf;
-	// Use this for initialization
-	void Start () {
-		var player = this.GetComponent<Player>();
-		_playerTransf = player.transform;
-	}
-	
+
+	public float speed = 0.3f;
+
 	// Update is called once per frame
 	void Update () {
-		_playerTransf.Rotate (0, 0, 2);
+
+		if ((speed < 3) && (Input.GetKey (KeyCode.W)))
+			speed += 0.1f;
+		if ((speed > -3) && (Input.GetKey (KeyCode.S)))
+			speed -= 0.1f;
+		Debug.Log (speed);
+		transform.Rotate (0, 0, speed);
 	}
 }
