aca405b627c55b49ab12869239418355d538ec54
heldersepu <heldersepu@gmail.com>
2014-11-22 15:55:52 +0000

    Add a simple 2D project

---
+++ 2D/Assets/Collision.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collision : MonoBehaviour {
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		GameObject obj = GameObject.Find("replayText");
+		obj.transform.Translate (0, 0, -2);
+	}
+}
