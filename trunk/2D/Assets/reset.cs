7f06519208378e79898ea69d8879daf5a0236477
heldersepu@gmail.com <heldersepu@gmail.com@72dac0d8-bfa9-6037-f185-37c1354a5d2f>
2014-11-22 15:55:52 +0000

    Add a simple 2D project

---
+++ trunk/2D/Assets/reset.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class reset : MonoBehaviour {
+	private GameObject[] players;
+	private Vector3[] vectors;
+
+	// Use this for initialization
+	void Start () {
+		players = GameObject.FindGameObjectsWithTag("Quad");
+		vectors = new Vector3[players.Length];
+		for (int i = 0; i < players.Length; i++)
+		{
+			vectors[i] = players[i].transform.position;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.anyKey) 
+		{
+			if (players.Length == 0) 
+			{
+				Debug.LogError ("No Player");
+			} else {
+				for (int i = 0; i < vectors.Length; i++)
+				{
+					players[i].transform.position = vectors[i];
+				}
+			}
+		}
+	}
+}
