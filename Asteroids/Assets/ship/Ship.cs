fc456df96d8b145639e10102c6cdd90db2bd7a1c
heldersepu <heldersepu@gmail.com>
2014-11-26 01:22:31 +0000

    Start on a 2D game asteroids

---
+++ Asteroids/Assets/ship/Ship.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ship : MonoBehaviour {
+	public float forwardSpeed = 0.12f;
+	public float rotationSpeed = 2.5f;
+
+	private float spd = 0;
+	private Animator anim;
+	void Start () {
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void FixedUpdate () {
+
+		int shift = 1;
+		if (Input.GetKey (KeyCode.LeftShift) || Input.GetKey (KeyCode.RightShift)) 
+			shift = 2;
+
+		if(Input.GetKeyUp(KeyCode.Space))
+		{
+			//rigidbody.AddForce(new Vector3 (0, 5, 0) * jumpPower, ForceMode.Impulse);
+		}
+
+
+		if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+			transform.Rotate (0, 0, rotationSpeed * shift);
+		if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+			transform.Rotate (0, 0, -rotationSpeed * shift);
+
+
+		bool fastAnim = false;
+		if (Input.GetKey (KeyCode.W) || Input.GetKey (KeyCode.UpArrow))
+		{
+			spd = forwardSpeed * shift;
+			fastAnim = true;
+		}
+		anim.SetBool ("Fast", fastAnim);
+
+		transform.Translate (spd, 0, 0);
+		spd *= 0.95f;
+	}
+}
