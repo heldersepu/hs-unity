7a91960d952b4c34323dad4642214969176fcb9b
heldersepu@gmail.com <heldersepu@gmail.com@72dac0d8-bfa9-6037-f185-37c1354a5d2f>
2014-12-05 01:44:26 +0000

    Latest changes to the asteroid project - Add aster prefab draw some asteroid - Asteroids explode on collision with the ship

--- trunk/Asteroids/Assets/screenRoll.cs
+++ trunk/Asteroids/Assets/screenRoll.cs
@@ -15,12 +15,7 @@ public class screenRoll : MonoBehaviour {
 		Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
 
 		if (pos.x < 0.0) // left of the camera's view
-		{
-			Debug.Log (transform.position);
-			Debug.Log (w);
 			transform.position = new Vector3 (transform.position.x + w, transform.position.y, transform.position.z); 
-
-		}
 		if (1.0 < pos.x) // right of the camera's view
 			transform.position = new Vector3 (transform.position.x - w, transform.position.y, transform.position.z);
 
