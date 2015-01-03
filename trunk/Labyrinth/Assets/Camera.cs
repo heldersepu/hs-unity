d57b5c6582d8b690c384c8aa112335dab3a24795
heldersepu <heldersepu@72dac0d8-bfa9-6037-f185-37c1354a5d2f>
2015-01-03 15:17:33 +0000

    Some improvements to the Movement on the labyrinth

--- trunk/Labyrinth/Assets/Camera.cs
+++ trunk/Labyrinth/Assets/Camera.cs
@@ -13,50 +13,77 @@ public class Camera : MonoBehaviour {
 		if (followBall) 
 		{
 			transform.position = ball.transform.position;
-			var rot = ball.transform.rotation.eulerAngles ; 
-			rot.z = 0;
-			//transform.rotation = Quaternion.Euler(rot);
+			transform.rotation = ball.transform.rotation;
 		}
+	}
+
+	void ChangeView () {
+		ball.SendMessage ("ChangeView");
+		var pos = transform.position;
+		pos.y = followBall ? 1 : 30;
+		transform.position = pos;
+		var rot = transform.rotation.eulerAngles ; 
+		rot.x = followBall ? 0 : 90;
+		if (!followBall) rot.y = 0;
+		transform.rotation = Quaternion.Euler(rot);
+	}
+
+	void ChaseBall () {
+		var pos = ball.transform.position;
+		pos.y = transform.position.y;
+		transform.position = Vector3.MoveTowards(transform.position, pos, 1);
+	}
+
+	void MoveCamera () {
+		if (Input.GetKey(KeyCode.Keypad8))
+			transform.Translate(0,speed,0);
+		
+		if (Input.GetKey(KeyCode.Keypad2))
+			transform.Translate(0,-speed,0);
+		
+		if (Input.GetKey(KeyCode.Keypad4))
+			transform.Translate(-speed,0,0);
+		
+		if (Input.GetKey(KeyCode.Keypad6))
+			transform.Translate(speed,0,0);
+		
+		if (Input.GetKey(KeyCode.Keypad5))
+			ChaseBall ();
+		
+		if (Input.GetKey(KeyCode.KeypadMinus))
+			if (transform.position.y > 5)
+				transform.Translate(0,0,speed);
+		
+		if (Input.GetKey(KeyCode.KeypadPlus))
+			if (transform.position.y < 50)
+				transform.Translate(0,0,-speed);
+	}
+
+	void AngleCamera () {
+		if (Input.GetKey(KeyCode.Keypad8))
+			transform.Rotate(-speed,0,0);			
+		
+		if (Input.GetKey(KeyCode.Keypad2))
+			transform.Rotate(speed,0,0);			
+		
+		if (Input.GetKey(KeyCode.Keypad4))
+			transform.Rotate(0,-speed,0);			
+		
+		if (Input.GetKey(KeyCode.Keypad6))
+			transform.Rotate(0,speed,0);
 	}
 	
 	void FixedUpdate () {
-		if(Input.GetKeyDown(KeyCode.V))
+		if (Input.GetKeyDown(KeyCode.V))
 		{
 			followBall = !followBall;
-			ball.SendMessage ("ChangeView");
-			var rot = transform.rotation.eulerAngles ; 
-			rot.z = followBall ? 0 : 90;
-			transform.rotation = Quaternion.Euler(rot);
+			ChangeView();
 		}
 		
 		if (!followBall)
-		{
-			if(Input.GetKey(KeyCode.Keypad8))
-				transform.Translate(0,speed,0);
-			
-			if(Input.GetKey(KeyCode.Keypad2))
-				transform.Translate(0,-speed,0);
-			
-			if(Input.GetKey(KeyCode.Keypad4))
-				transform.Translate(-speed,0,0);
-			
-			if(Input.GetKey(KeyCode.Keypad6))
-				transform.Translate(speed,0,0);
-			
-			if(Input.GetKey(KeyCode.Keypad5))
-			{
-				var pos = ball.transform.position;
-				if (pos.y < 10) pos.y = 10;
-				transform.position = Vector3.MoveTowards (transform.position, pos, 1);
-			}
-			
-			if(Input.GetKey(KeyCode.KeypadMinus))
-				if (transform.position.y > 5)
-					transform.Translate(0,0,speed);
-			
-			if(Input.GetKey(KeyCode.KeypadPlus))
-				if (transform.position.y < 50)
-					transform.Translate(0,0,-speed);
-		}
+			if (Input.GetKey(KeyCode.LeftAlt) || Input.GetKey(KeyCode.RightAlt))
+				AngleCamera();
+			else
+				MoveCamera();
 	}
 }
