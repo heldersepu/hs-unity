2647177b668b0f71814a5fe6ab9fc5bfec761ce5
HelderSepu <heldersepu@gmail.com>
2017-08-05 20:53:02 -0400

    Add project BallControl
    Add a Simple project showing how to control a ball

---
+++ BallControl/Assets/Movement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour {
+    public float force = 10f;
+
+    Rigidbody rigBody { get { return GetComponent<Rigidbody>(); } }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKey(KeyCode.UpArrow))
+            rigBody.AddForce(Vector3.forward * force);
+
+        if (Input.GetKey(KeyCode.DownArrow))
+            rigBody.AddForce(Vector3.back * force);
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+            rigBody.AddForce(Vector3.left * force);
+
+        if (Input.GetKey(KeyCode.RightArrow))
+            rigBody.AddForce(Vector3.right * force);
+    }
+}
